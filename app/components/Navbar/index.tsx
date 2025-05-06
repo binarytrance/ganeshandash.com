c22ab3f89ad32136a197e09dee95a1a3bc58cd9a
binarytrance <ganeshan.dash@com>
2025-05-07 00:34:04 +0530

    install react-icons

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,12 +1,24 @@
+import { BiLogoGithub } from "react-icons/bi";
+
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navbar__site-links">
+    <nav className="navbar flex justify-between items-center mx-[10vw] py-8">
+      <div className="navbar__site-links gap-4 flex">
         <a href="#home">Home</a>
         <a href="#about">About</a>
         <a href="#contact">Contact</a>
       </div>
-      <div className="navbar__contact-links"></div>
+      <div className="navbar__contact-links flex gap-4 items-center">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/binarytrance/"
+        >
+          <BiLogoGithub />
+        </a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+      </div>
     </nav>
   );
 }
