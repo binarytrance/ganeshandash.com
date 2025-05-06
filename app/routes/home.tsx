4fdd9d8c3f482ac471c3a8267d7cfad4102be27d
binarytrance <ganeshan.dash@com>
2025-05-06 19:25:57 +0530

    re-organising existing files into folders

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from "../components/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Ganeshan's World!" },
+    {
+      name: "description",
+      content: "Frontend developer and creative software engineer!",
+    },
   ];
 }
 
