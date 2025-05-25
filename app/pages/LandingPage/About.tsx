f6c08b5bf0dac71bb8dbbe280a3dd3c4b8801b0a
binarytrance <ganeshan.dash@com>
2025-05-26 01:52:10 +0530

    fix a bug with the hero image. update blog publishing date

--- app/pages/LandingPage/About.tsx
+++ app/pages/LandingPage/About.tsx
@@ -9,7 +9,7 @@ const About = ({}) => {
       <SVG name="hexagon" className="hero-pattern h-100" />
       <Section id={"about"} className="gap-10 align-center border-gold">
         <div className="flex flex-col sm:flex-row items-center gap-6 sm:gap-12">
-          <div className="flex justify-center align-center w-[120px] h-[120px] sm:w-[200px] sm:h-[200px] border-3 border-gold rounded-full border-dotted">
+          <div className="flex justify-center items-center w-[120px] h-[120px] sm:w-[200px] sm:h-[200px] border-3 border-gold rounded-full border-dotted">
             <img
               src="/images/self-potrait.png"
               alt="Ganeshan Dash"
--- app/pages/LandingPage/LatestBlogs.tsx
+++ app/pages/LandingPage/LatestBlogs.tsx
@@ -6,7 +6,7 @@ const LatestBlogs = () => {
       <h2 className="font-serif text-2xl sm:text-4xl text-gold letter mb-6 sm:mb-12">
         Latest Blogs
       </h2>
-      <p>First of many, dropping on: 19th May, 2025</p>
+      <p>First of many, dropping on: 5th June, 2025</p>
     </Section>
   );
 };
